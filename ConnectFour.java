6d8b408beaf6fef20220bcbb12bc1dadea9b5aa5
TottyAmadeus <pedrosousa084@hotmail.com>
2023-03-29 15:16:14 +0100

    agora aceita o MCTS

--- ConnectFour.java
+++ ConnectFour.java
@@ -5,7 +5,7 @@ public class ConnectFour {
         Scanner stdin = new Scanner(System.in);
         Board inicial = new Board();
 
-        switch (Args[0]) {
+        switch ("MCTS") {
 
             case "PvP":
                 for (int i = 0; i < 42; i++) {
@@ -19,6 +19,41 @@ public class ConnectFour {
                 
                 }
                 break;
+
+            case "MCTS":
+
+            boolean turn = true;
+
+                Board current = inicial;
+
+                for (int i = 0; i < 42; i++) {
+
+                    if(turn){ // player
+
+                        int col = stdin.nextInt() - 1;
+                        Board newB = current.makeMove(col);
+                        newB.setParent(current);
+                        newB.printBoard();
+
+                        if (newB.isWinner(col)) {
+                            System.out.println("O Jogador Ganhou !");
+                            break;
+                        }
+
+                        current = newB;
+                        turn = !turn;
+
+                    }else{ // MCTS
+                        MCTS MonteCarlo = new MCTS(current, true, null);
+                        Board newB = MonteCarlo.montecarlo();
+                        newB.printBoard();
+                        current = newB;
+                        turn = !turn;
+                    }
+
+                }
+                break;
+
                 
 
         }
